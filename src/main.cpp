5e3a777a6cf5fcea7cf122ed7d4e6ab64f659f3b
Alex Landherr <alex.landherr@gmail.com>
2023-09-04 20:46:47 +0000

    Added problem description as block comment in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/*
+All integers under 10 that are multiples of 3 or 5 are 3 5 6 9.
+
+The sum of these is 23. What is the sum of the multiples of 3 or 5 under 1000?
+*/
+
 int main() {
 
     return 0;
